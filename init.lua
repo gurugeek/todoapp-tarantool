ca495f7b33c13a0db78e23c0fd4ce600b0334692
gurugeek <gurugeek@mac.com>
2018-02-24 19:40:23 +0100

    lua change

--- init.lua
+++ init.lua
@@ -3,9 +3,6 @@
 --- DateTime: 23.02.18 20:00
 ---
 
-box.schema.user.create('todo', {password='test', if_not_exists=true})
-box.schema.user.grant('todo', 'read,write', 'space', 'todo', {if_not_exists=true})
-
 if box.space.todo == nil then
     box.schema.create_space('todo')
     box.space.todo:format({{name='id', type='unsigned'}, {name='title', type='string'}, {name='completed', type='boolean'}, {name='created', type='integer'},{name='owner', type='string'}})
@@ -13,3 +10,6 @@ if box.space.todo == nil then
     box.space.todo:create_index('created', {type='tree', parts={4, 'integer'}})
     box.space.todo:create_index('owner', {type='tree', parts={5, 'string'}, unique=false})
 end
+
+box.schema.user.create('todo', {password='test', if_not_exists=true})
+box.schema.user.grant('todo', 'read,write', 'space', 'todo', {if_not_exists=true})
